defb07a3fc5570398622234f733baa151533e3b3
sin-k <son_k0131@icloud.com>
2024-08-06 12:38:03 +0900

    敵の消滅判定用のプログラムを消滅するときにエフェクトを出すように変更

--- Assets/Kawakubo/EnemyDamage.cs
+++ Assets/Kawakubo/EnemyDamage.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
+    [SerializeField] ParticleSystem _particles;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var Attacker = collision.GetComponentInParent<PlayerAttack>();
         if (Attacker != null && !Attacker.CanAttack && collision.tag == "Weapon")
         {
+            _particles.Play();
             Destroy(this.gameObject);
         }
     }
