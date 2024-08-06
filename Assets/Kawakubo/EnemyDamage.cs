6f23a3a090f17b3e37b229698591351c7d1fc43c
sin-k <son_k0131@icloud.com>
2024-08-06 13:39:13 +0900

    敵の消滅判定用プログラムの修正

--- Assets/Kawakubo/EnemyDamage.cs
+++ Assets/Kawakubo/EnemyDamage.cs
@@ -7,10 +7,13 @@ public class EnemyDamage : MonoBehaviour
     [SerializeField] ParticleSystem _particles;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var Attacker = collision.GetComponentInParent<PlayerAttack>();
-        if (Attacker != null && !Attacker.CanAttack && collision.tag == "Weapon")
+        var Attacker = collision.GetComponent<PlayerAttack>();
+        if (Attacker != null && !Attacker.CanAttack && collision.gameObject.tag == "Weapon")
         {
-            _particles?.Play();
+            if(_particles != null)
+            {
+                _particles.Play();
+            }
             Destroy(this.gameObject);
         }
     }
