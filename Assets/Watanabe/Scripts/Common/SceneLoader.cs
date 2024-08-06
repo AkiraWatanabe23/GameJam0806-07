f3e3c9731a18d470239a65e393027ba2677ff8e0
aki_ra <a0323w@gmail.com>
2024-08-06 09:28:20 +0900

    [add] MyTemplate

---
+++ Assets/Watanabe/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using Constants;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader
+{
+    /// <summary> フェード -> シーン遷移 </summary>
+    public static void FadeLoad(SceneName scene) => Fade.Instance.StartFadeOut(() => LoadToScene(scene));
+
+    /// <summary> シーン遷移 </summary>
+    public static void LoadToScene(SceneName scene) => SceneManager.LoadScene(Consts.Scenes[scene]);
+}
---
+++ Assets/Watanabe/Scripts/Others/SingletonMonoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary> SingletonMonoBehaviourの基底クラス </summary>
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected new abstract bool DontDestroyOnLoad { get; }
+
+    private static T _instance = default;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Type type = typeof(T);
+                _instance = (T)FindObjectOfType(type);
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        if (DontDestroyOnLoad) { DontDestroyOnLoad(gameObject); }
+    }
+}
