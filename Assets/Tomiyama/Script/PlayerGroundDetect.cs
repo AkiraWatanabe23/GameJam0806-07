23e8c661deecdb4dcd3aaa91ba236df4c7d4457e
AoiTomiyama <TadoO.o2209@gmail.com>
2024-08-07 15:14:44 +0900

    プレイヤーの接地検知を名前からタグで検知するように変更。

--- Assets/Tomiyama/Script/PlayerGroundDetect.cs
+++ Assets/Tomiyama/Script/PlayerGroundDetect.cs
@@ -11,7 +11,7 @@ public class PlayerGroundDetect : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name.Contains("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             _anim.SetBool("IsGrounded", true);
             _playerMove.JumpCount = 2;
